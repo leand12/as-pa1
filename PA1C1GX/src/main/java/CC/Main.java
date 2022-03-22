26253da992012d5a28c8fb7abc853e58d20866fe
Margarida <margarida.martins@ua.pt>
2022-03-22 00:30:05 +0000

    very simple sockect comunication

--- PA1C1GX/src/main/java/CC/Main.java
+++ PA1C1GX/src/main/java/CC/Main.java
@@ -4,10 +4,44 @@
  */
 package CC;
 
+import CC.GUI.GUI;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
 /**
  *
  * @author guids
  */
 public class Main {
+    
+    final static int portNumber = 5000;
+    final static String hostName = "127.0.0.1";
+    
+    public static void main(String[ ] args) {
+        
+        GUI gui = new GUI();
+        gui.setVisible(true);
+        
+        System.out.println("CC begining:");
+        
+        try{ 
+            Socket sock = new Socket(hostName, portNumber);
+            System.out.println(sock.getInetAddress());
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            BufferedReader in = new BufferedReader( new InputStreamReader(sock.getInputStream()));
+            
+            String inputLine;
+            out.println("Hello I'm CC");
+            while ((inputLine = in.readLine()) != null) {               
+                System.out.println(inputLine);
+            }
+        }
+        catch(Exception e){
+
+        }
+    }
     
 }
--- PA1C1GX/src/main/java/HC/Main.java
+++ PA1C1GX/src/main/java/HC/Main.java
@@ -4,10 +4,42 @@
  */
 package HC;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author guids
  */
 public class Main {
     
+    final static int portNumber = 5000;
+    
+    public static void main(String[ ] args) {
+        
+        System.out.println("HC begining:");
+        
+        try{ 
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            System.out.println(clientSocket.getLocalSocketAddress());
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader( new InputStreamReader(clientSocket.getInputStream()));
+            
+            String inputLine;
+            out.println("Hello, this is HC");
+            while ((inputLine = in.readLine()) != null) {               
+                System.out.println(inputLine);
+            }
+        }
+        catch(Exception e){
+
+        }
+    }
+    
+    
+
 }
