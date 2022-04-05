c81e988b0ffbccee80a673c8bb65dea54a699a66
Margarida <margarida.martins@ua.pt>
2022-04-05 19:29:20 +0100

    logging implementation

---
+++ PA1C1GX/src/main/java/HC/Logging/Logging.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HC.Logging;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author guids
+ */
+public class Logging {
+    
+    private BufferedWriter logfile; 
+    private ReentrantLock rl;
+    private Condition cWrite;
+    
+    private boolean isWriting = false;
+    
+    public Logging() throws IOException{
+        this.logfile = new BufferedWriter(new FileWriter("src/main/java/HC/Logging/log.txt")); 
+        this.rl = new ReentrantLock();
+        this.cWrite = rl.newCondition();
+
+    }
+    
+    public void log(String message) throws IOException{
+        try {
+            rl.lock();
+            while(this.isWriting){
+                cWrite.await();
+            }
+            this.isWriting = true;
+            System.out.println(message);
+            this.logfile.write(message);
+            this.logfile.newLine();
+            this.logfile.flush();
+            this.cWrite.signal();
+            this.isWriting = false;
+            
+              
+        } catch ( InterruptedException ex ) {return;}
+        finally {
+            rl.unlock();
+            
+        }
+        
+    }
+    
+}
